82d1d04b19b631a25e9121e8b0840f163e6c4909
euna <christinejung10@daum.net>
2021-10-12 16:43:13 +0900

    connect 1st page & 2nd page

--- js/greetings.js
+++ js/greetings.js
@@ -2,6 +2,11 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+const intro_clock = document.querySelector("#intro .clock");
+const main = document.querySelector("#main");
+// const quote = document.querySelector("#quote");
+const container = document.querySelector("#container");
+
 // repeating strings
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "userName";
@@ -12,16 +17,16 @@ if (savedUserName === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
+    paint2ndPage();
     paintGreetings(savedUserName);
 }
 
-onLoginSubmit(); // 임시
 // get userName
 function onLoginSubmit(event) {
-    // event.preventDefault(); // 엔터 후 새로고침 방지 (고유동작 중지)
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    // const userName = loginInput.value;
-    const userName = "euna"; // 임시
+    event.preventDefault(); // 엔터 후 새로고침 방지 (고유동작 중지)
+    paint2ndPage();
+    const userName = loginInput.value;
+    console.log(userName);
     localStorage.setItem(USERNAME_KEY, userName); // save in local storage
     paintGreetings(userName);
 }
@@ -29,4 +34,12 @@ function onLoginSubmit(event) {
 function paintGreetings(userName) {
     greeting.innerText = `Hello ${userName}`; // "Hello " + userName
     greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function paint2ndPage() {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    intro_clock.classList.add(HIDDEN_CLASSNAME);
+    main.classList.remove(HIDDEN_CLASSNAME);
+    quote.classList.remove(HIDDEN_CLASSNAME);
+    container.classList.remove(HIDDEN_CLASSNAME);
 }
